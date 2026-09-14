aba65fd415a5aabc4b21d2c0cc789429bd6831bd
Luiz Henrique Saldanha de França <luiz.saldanha@codise.gov-se>
2026-09-14 12:00:01 -0300

    feat: adicionar migrations do projeto

---
+++ Migrations/20260914145048_AddAnimeDiretor.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TesteTecnico.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddAnimeDiretor : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Diretores",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Nome = table.Column<string>(type: "text", nullable: false),
+                    DataNascimento = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Diretores", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Animes",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Nome = table.Column<string>(type: "text", nullable: false),
+                    Descricao = table.Column<string>(type: "text", nullable: false),
+                    AnoLancamento = table.Column<int>(type: "integer", nullable: false),
+                    NumeroEpisodios = table.Column<int>(type: "integer", nullable: false),
+                    DiretorId = table.Column<Guid>(type: "uuid", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Animes", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Animes_Diretores_DiretorId",
+                        column: x => x.DiretorId,
+                        principalTable: "Diretores",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Animes_DiretorId",
+                table: "Animes",
+                column: "DiretorId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Animes");
+
+            migrationBuilder.DropTable(
+                name: "Diretores");
+        }
+    }
+}
