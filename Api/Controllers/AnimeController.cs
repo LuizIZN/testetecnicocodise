5a9d5c154e980faa959dc6533cecb5e599c25398
Luiz <saldanhalhs.lh87@gmail.com>
2026-09-16 10:58:11 -0300

    feat: adicionar endpoint de get anime by id

--- Api/Controllers/AnimeController.cs
+++ Api/Controllers/AnimeController.cs
@@ -1,7 +1,8 @@
-namespace TesteTecnico.Application.Controllers;
+namespace TesteTecnico.Api.Controllers;
 using Microsoft.AspNetCore.Mvc;
 using TesteTecnico.Application.Interfaces;
 using TesteTecnico.Application.Dtos;
+using TesteTecnico.Domain.Models;
 
 [ApiController]
 [Route("anime")]
@@ -10,17 +11,31 @@ public sealed class AnimeController(IAnimeService animeService) : ControllerBase
     private readonly IAnimeService _animeService = animeService;
 
     [HttpPost]
-    public ActionResult<string> Create(CreateAnimeRequest request)
+    public async Task<ActionResult<Guid>> Create(CreateAnimeRequest request)
     {
         try
         {
-            var result = _animeService.AddAsync(request);
+            var result = await _animeService.AddAsync(request);
 
-            return CreatedAtAction(nameof(result.Id), new { id = result.Id }, "Anime criado com sucesso.");
+            return CreatedAtAction(nameof(GetById), new { id = result.Id }, result.Id);
         }
         catch (Exception ex)
         {
             return BadRequest(ex.Message);
         }
     } 
+
+    [HttpGet("{id}")]
+    public async Task<ActionResult<GetAnimeResponse>> GetById(Guid id)
+    {
+        try
+        {
+            var anime = await _animeService.GetByIdAsync(id);
+            return Ok(anime);
+        }
+        catch (Exception ex)
+        {
+            return NotFound(ex.Message);
+        }
+    }
 }
