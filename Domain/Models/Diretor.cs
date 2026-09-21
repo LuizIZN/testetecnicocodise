cdac5893c51210b31b2298c56d00d4aa28e824c5
Luiz <saldanhalhs.lh87@gmail.com>
2026-09-21 09:53:22 -0300

    fix: voltar a listar animes por diretor

--- Domain/Models/Diretor.cs
+++ Domain/Models/Diretor.cs
@@ -5,6 +5,7 @@ public class Diretor(string Nome, DateOnly DataNascimento)
     public Guid Id { get; private set; }
     public string Nome { get; private set; } = Nome;
     public DateOnly DataNascimento { get; private set; } = DataNascimento;
+    public List<Anime> Animes { get; private set; } = [];
 
     public void Update(Diretor updatedDiretor)
     {
