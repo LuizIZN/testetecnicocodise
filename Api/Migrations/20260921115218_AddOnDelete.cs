4783329165f924c717a3381d64286591e92a9798
Luiz <saldanhalhs.lh87@gmail.com>
2026-09-21 08:53:22 -0300

    feat: adicionar restrição de deleção no realacionamento

---
+++ Api/Migrations/20260921115218_AddOnDelete.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TesteTecnico.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddOnDelete : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Animes_Diretores_DiretorId",
+                table: "Animes");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Animes_Diretores_DiretorId",
+                table: "Animes",
+                column: "DiretorId",
+                principalTable: "Diretores",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Animes_Diretores_DiretorId",
+                table: "Animes");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Animes_Diretores_DiretorId",
+                table: "Animes",
+                column: "DiretorId",
+                principalTable: "Diretores",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
