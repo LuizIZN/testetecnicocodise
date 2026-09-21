1e4d1c2511dddfc1abf85d270e437ff1fe5d360e
Luiz <saldanhalhs.lh87@gmail.com>
2026-09-21 08:49:12 -0300

    fix: mudar tipo de data nascimento para dateonly

--- Api/Seeders/DiretorSeeder.cs
+++ Api/Seeders/DiretorSeeder.cs
@@ -13,16 +13,16 @@ public class DiretorSeeder(AppDbContext context)
         {
             var diretores = new List<Diretor>
             {
-                new(Nome: "Hayao Miyazaki", DataNascimento: "1941-01-05"), 
-                new(Nome: "Makoto Shinkai", DataNascimento: "1973-02-09"), 
-                new(Nome: "Mamoru Hosoda", DataNascimento: "1967-09-19"), 
-                new(Nome: "Satoshi Kon", DataNascimento: "1963-10-12"), 
-                new(Nome: "Isao Takahata", DataNascimento: "1935-10-29"), 
-                new(Nome: "Yoshiyuki Tomino", DataNascimento: "1941-11-05"), 
-                new(Nome: "Shinichiro Watanabe", DataNascimento: "1965-05-24"), 
-                new(Nome: "Hideaki Anno", DataNascimento: "1960-05-22"), 
-                new(Nome: "Katsuhiro Otomo", DataNascimento: "1954-04-14"), 
-                new(Nome: "Mamoru Oshii", DataNascimento: "1951-08-08")
+                new(Nome: "Hayao Miyazaki", DataNascimento: DateOnly.Parse("1941-01-05")), 
+                new(Nome: "Makoto Shinkai", DataNascimento: DateOnly.Parse("1973-02-09")), 
+                new(Nome: "Mamoru Hosoda", DataNascimento: DateOnly.Parse("1967-09-19")), 
+                new(Nome: "Satoshi Kon", DataNascimento: DateOnly.Parse("1963-10-12")), 
+                new(Nome: "Isao Takahata", DataNascimento: DateOnly.Parse("1935-10-29")), 
+                new(Nome: "Yoshiyuki Tomino", DataNascimento: DateOnly.Parse("1941-11-05")), 
+                new(Nome: "Shinichiro Watanabe", DataNascimento: DateOnly.Parse("1965-05-24")), 
+                new(Nome: "Hideaki Anno", DataNascimento: DateOnly.Parse("1960-05-22")), 
+                new(Nome: "Katsuhiro Otomo", DataNascimento: DateOnly.Parse("1954-04-14")), 
+                new(Nome: "Mamoru Oshii", DataNascimento: DateOnly.Parse("1951-08-08"))
             };
 
             _context.Diretores.AddRange(diretores);
