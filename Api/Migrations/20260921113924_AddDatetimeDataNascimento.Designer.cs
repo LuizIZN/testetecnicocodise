ba3e403939862262a8408b67dac3cdf40f89bc00
Luiz <saldanhalhs.lh87@gmail.com>
2026-09-21 08:48:29 -0300

    fix: mudar tipo de data nascimento para dateonly

--- Api/Migrations/20260921113924_AddDatetimeDataNascimento.Designer.cs
+++ Api/Migrations/20260921113924_AddDatetimeDataNascimento.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using TesteTecnico.Infraestructure.Data;
@@ -11,9 +12,11 @@ using TesteTecnico.Infraestructure.Data;
 namespace TesteTecnico.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260921113924_AddDatetimeDataNascimento")]
+    partial class AddDatetimeDataNascimento
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Api/Migrations/20260921113924_AddDatetimeDataNascimento.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TesteTecnico.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddDatetimeDataNascimento : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("""
+                ALTER TABLE "Diretores"
+                ALTER COLUMN "DataNascimento" TYPE date
+                USING "DataNascimento"::date;
+                """);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("""
+                ALTER TABLE "Diretores"
+                ALTER COLUMN "DataNascimento" TYPE text
+                USING "DataNascimento"::text;
+                """);
+        }
+    }
+}
